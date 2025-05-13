04d0abdf16ab7574b3acc5356ceac879bb79fbbc
Cristian <c.isaacdominguez@gmail.com>
2025-05-13 13:23:39 -0700

    update index.ts with artwork router

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import { PORT } from './config/env';
 import artistRoutes from './modules/artists/routes';
+import artworkRoutes from './modules/artworks/routes';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/artists', artistRoutes);
+app.use('/api/artworks', artworkRoutes);
 
 app.listen(PORT || 3000, () => {
   console.log(`Server running on port ${PORT || 3000}`);
