22179cf0925f0d70c4e78fbc391b3853fdb698ce
Cristian <c.isaacdominguez@gmail.com>
2025-05-14 11:42:29 -0700

    organize file

--- src/modules/artists/controller.ts
+++ src/modules/artists/controller.ts
@@ -20,37 +20,6 @@ export const getArtistsHandler = async (req: Request, res: Response) => {
   }
 };
 
-export const getArtistByIdHandler = async (
-  req: Request<{ id: string }>,
-  res: Response
-) => {
-  // get id param and verify that it's a number
-  const parsedId = Number(req.params.id);
-  if (Number.isNaN(parsedId)) {
-    res.status(400).json({ error: 'Invalid artist ID.' });
-    return;
-  }
-
-  try {
-    const artist = await getArtistById(parsedId);
-
-    if (!artist) {
-      res
-        .status(404)
-        .json({ error: `Artist with id ${parsedId} was not found.` });
-      return;
-    }
-
-    res.status(200).json({ data: artist });
-  } catch (error) {
-    console.log(`Error getting artist with id ${parsedId}:`, error);
-    res
-      .status(500)
-      .json({ error: `Internal error getting artist with id ${parsedId}.` });
-    return;
-  }
-};
-
 export const getRandomArtistHandler = async (req: Request, res: Response) => {
   try {
     const artist = await getRandomArtist();
@@ -100,5 +69,36 @@ export const getFullArtistInfoHandler = async (req: Request, res: Response) => {
     res.status(500).json({
       error: `Internal error getting full info for artist with id ${parsedId}`,
     });
+  }
+};
+
+export const getArtistByIdHandler = async (
+  req: Request<{ id: string }>,
+  res: Response
+) => {
+  // get id param and verify that it's a number
+  const parsedId = Number(req.params.id);
+  if (Number.isNaN(parsedId)) {
+    res.status(400).json({ error: 'Invalid artist ID.' });
+    return;
+  }
+
+  try {
+    const artist = await getArtistById(parsedId);
+
+    if (!artist) {
+      res
+        .status(404)
+        .json({ error: `Artist with id ${parsedId} was not found.` });
+      return;
+    }
+
+    res.status(200).json({ data: artist });
+  } catch (error) {
+    console.log(`Error getting artist with id ${parsedId}:`, error);
+    res
+      .status(500)
+      .json({ error: `Internal error getting artist with id ${parsedId}.` });
+    return;
   }
 };
