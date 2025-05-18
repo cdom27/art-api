af1b31b56f9ee352f1aa94640046c41acfb595cd
Cristian <c.isaacdominguez@gmail.com>
2025-05-18 12:45:32 -0700

    update 404 page

--- client/src/routes/NotFound.tsx
+++ client/src/routes/NotFound.tsx
@@ -3,7 +3,7 @@ import SiteLayout from '../layouts/SiteLayout';
 export default function NotFound() {
   return (
     <SiteLayout>
-      <h1 className='text-4xl'>Something about artists</h1>
+      <h1 className='text-4xl'>404</h1>
     </SiteLayout>
   );
 }
