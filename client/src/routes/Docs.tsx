3bc46d84a863ca22ef62b981d3afc0198a194aff
Cristian <c.isaacdominguez@gmail.com>
2025-05-17 21:58:26 -0700

    implement register hook, form, validations and api call

--- client/src/routes/Docs.tsx
+++ client/src/routes/Docs.tsx
@@ -1,3 +1,4 @@
+import RegisterForm from '../components/forms/RegisterForm';
 import SiteLayout from '../layouts/SiteLayout';
 
 export default function Docs() {
@@ -8,21 +9,7 @@ export default function Docs() {
       <h2 className='text-3xl'>Generate Key</h2>
       <p>Register your website to generate a public api key</p>
 
-      <form className='flex items-center gap-2'>
-        <label htmlFor='url'>Your website</label>
-
-        <input
-          type='url'
-          id='url'
-          name='url'
-          placeholder='https://cidominguez.com'
-          className='border-2 border-black'
-        />
-
-        <button type='submit' className='bg-black text-white py-1 px-3'>
-          Generate Key
-        </button>
-      </form>
+      <RegisterForm />
     </SiteLayout>
   );
 }
