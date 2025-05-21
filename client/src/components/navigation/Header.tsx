947ce1ebe3d48a0b924ec1a8aa5e921752f6d320
Cristian <c.isaacdominguez@gmail.com>
2025-05-21 11:43:42 -0700

    add github button

--- client/src/components/navigation/Header.tsx
+++ client/src/components/navigation/Header.tsx
@@ -2,21 +2,6 @@ import { Link } from 'react-router';
 import Logo from '../../assets/logo.webp';
 
 export default function Header() {
-  const navLinks = [
-    {
-      name: 'Explore',
-      href: '/explore',
-    },
-    {
-      name: 'Developers',
-      href: '/docs',
-    },
-    {
-      name: 'About',
-      href: '/about',
-    },
-  ];
-
   return (
     <header className='flex items-center px-3 py-6 justify-between'>
       <div className='flex items-center gap-2'>
@@ -25,13 +10,16 @@ export default function Header() {
       </div>
 
       <nav className='flex items-center gap-4'>
-        {navLinks.map((link) => {
-          return (
-            <Link key={link.name} to={link.href}>
-              {link.name}
-            </Link>
-          );
-        })}
+        <Link
+          className='github-button'
+          to='https://github.com/cdom27/influential-artists-api'
+          data-color-scheme='no-preference: dark; light: dark; dark: dark;'
+          data-icon='octicon-star'
+          data-size='large'
+          aria-label='Star cdom27/influential-artists-api on GitHub'
+        >
+          Star on GitHub
+        </Link>
       </nav>
     </header>
   );
