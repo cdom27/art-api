00a73d48ae225689d5d89dc2de53540247eac7de
Cristian <c.isaacdominguez@gmail.com>
2025-05-21 16:30:37 -0700

    remove proxy/host config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,12 +5,4 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // server: {
-  //   proxy: {
-  //     '/api': 'http://localhost:3000',
-  //   },
-  //   allowedHosts: [
-  //     '8f52-2607-fb91-8292-4bc5-4c17-96f3-f9db-ff44.ngrok-free.app',
-  //   ],
-  // },
 });
