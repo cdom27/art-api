f7727bbd9e4341809a29be51c3d08354e569211f
Cristian <c.isaacdominguez@gmail.com>
2025-05-24 22:04:34 -0700

    update env var loading

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,10 +2,18 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const { DB_URL, PORT } = process.env;
+// for CI/CD environments, we use TEST_DB_URL for NODE_ENV=test
+// for local development, we use DB_URL
+const dbUrl =
+  process.env.NODE_ENV === 'test'
+    ? process.env.TEST_DB_URL || process.env.DB_URL
+    : process.env.DB_URL;
+
+export const DB_URL = dbUrl;
+export const { PORT } = process.env;
 
 if (!DB_URL) {
   throw new Error(
-    'Database env vars are not fully defined (missing DB_URL or individual credentials)'
+    'Database env vars are not fully defined (missing DB_URL or TEST_DB_URL)',
   );
 }
